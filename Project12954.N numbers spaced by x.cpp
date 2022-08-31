1db5cfda91f074879b8aed88626b12592e89340f
404Vector-mac <tiryul@gmail.com>
2022-08-31 11:30:04 +0900

    add project - n numbers spaced by x

---
+++ Project12954.N numbers spaced by x.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer(n);
+    for(long long i = 0; i != n; i++) answer[i] = x * (i+1);
+    return answer;
+}
